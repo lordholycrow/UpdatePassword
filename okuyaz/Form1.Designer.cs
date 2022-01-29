58f70f9df8d3b2f40163b88f81daa58231feb50b
Muhammed Ali Sarimese <56533860+lordholycrow@users.noreply.github.com>
2022-01-29 15:09:27 +0100

    Add files via upload

---
+++ okuyaz/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace okuyaz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button29 = new System.Windows.Forms.Button();
+            this.button30 = new System.Windows.Forms.Button();
+            this.button47 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button29
+            // 
+            this.button29.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button29.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button29.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button29.Location = new System.Drawing.Point(-1, 125);
+            this.button29.Name = "button29";
+            this.button29.Size = new System.Drawing.Size(199, 61);
+            this.button29.TabIndex = 29;
+            this.button29.Text = "Şifre Güncelle";
+            this.button29.UseVisualStyleBackColor = false;
+            // 
+            // button30
+            // 
+            this.button30.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button30.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button30.Location = new System.Drawing.Point(-1, 30);
+            this.button30.Name = "button30";
+            this.button30.Size = new System.Drawing.Size(199, 61);
+            this.button30.TabIndex = 28;
+            this.button30.Text = "Aktar";
+            this.button30.UseVisualStyleBackColor = false;
+            // 
+            // button47
+            // 
+            this.button47.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button47.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button47.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button47.Location = new System.Drawing.Point(420, 516);
+            this.button47.Name = "button47";
+            this.button47.Size = new System.Drawing.Size(199, 61);
+            this.button47.TabIndex = 45;
+            this.button47.Text = "Şifre Güncelle";
+            this.button47.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(283, 260);
+            this.Controls.Add(this.button47);
+            this.Controls.Add(this.button29);
+            this.Controls.Add(this.button30);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button29;
+        private System.Windows.Forms.Button button30;
+        private System.Windows.Forms.Button button47;
+    }
+}
+
---
+++ okuyaz/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace okuyaz
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglan = new SqlConnection("Data Source=.; Initial Catalog=.; User Id=.; password=.");
+        SqlConnection baglan2 = new SqlConnection("Data Source=.; Initial Catalog=.; User Id=.; password=.");
+        DataTable dt = new DataTable();
+        DataTable dt2 = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = EASEncrypt("123456");
+
+        }
+        ///////////////////////////////////////////////////////////
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //da.Fill(ds, "Mellivo_PrimPuan");
+            //dataGridView1.DataSource = ds.Tables["Mellivo_PrimPuan"];
+            baglan.Open();
+            SqlDataAdapter da = new SqlDataAdapter("Select Top 10000 * From table_name", baglan);
+            DataSet ds = new DataSet();
+            baglan.Close();
+            da.Fill(dt);
+
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+
+                baglan2.Open();
+
+                string ekle = "insert into PrimPuan(DbRow1,DbRow2,DbRow3,DbRow4,DbRow5,DbRow6) values (@DbRow1,@DbRow2,@DbRow3,@DbRow4,@DbRow5,@DbRow6)";
+
+                SqlCommand komut = new SqlCommand(ekle, baglan2);
+
+                komut.Parameters.AddWithValue("@DbRow1", dt.Rows[i][0]);
+                komut.Parameters.AddWithValue("@DbRow2", dt.Rows[i][1]);
+                komut.Parameters.AddWithValue("@DbRow3", dt.Rows[i][2]);
+                komut.Parameters.AddWithValue("@DbRow4", dt.Rows[i][3]);
+                komut.Parameters.AddWithValue("@DbRow5", dt.Rows[i][4]);
+                komut.Parameters.AddWithValue("@DbRow6", dt.Rows[i][5]);
+                komut.ExecuteNonQuery();
+
+                baglan2.Close();
+            }
+
+            MessageBox.Show("Aktarma işlemi başarıyla tamamlandı.");
+        }
+        ///////////////////////////////////////////////////////////
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglan2.Open();
+            SqlDataAdapter da2 = new SqlDataAdapter("Select Top 10 DbRow2,DbRow1 From table_name", baglan2);
+            DataSet ds2 = new DataSet();
+            baglan2.Close();
+            da2.Fill(dt2);
+
+            for (int i = 0; i < dt2.Rows.Count; i++)
+            {
+                baglan2.Open();
+                string güncelle = "UPDATE table_name set DbRow2=@DbRow2 where DbRow1 = " + dt2.Rows[i][1].ToString();
+                SqlCommand komut = new SqlCommand(güncelle, baglan2);
+                komut.Parameters.AddWithValue("@DbRow2", EASEncrypt(dt2.Rows[i][0].ToString()));
+
+                komut.ExecuteNonQuery();
+                baglan2.Close();
+            }
+            MessageBox.Show("Şifre güncelleme işlemi başarıyla tamamlandı.");
+        }
+
+        public string EASEncrypt(string clearText)
+        {
+            string EncryptionKey = "FRMTDS2016IASDASDS99";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x76, 0x49, 0x65, 0x64, 0x20, 0x61, 0x76, 0x64, 0x76, 0x65, 0x6e, 0x65, 0x4d });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+    }
+}
+
